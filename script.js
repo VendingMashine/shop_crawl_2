1b3072cd58e60000caa50937deea9d444f8cd653
Cheikh Seck <jvmstix@gmail.com>
2021-10-28 08:30:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+var request_lib = require()
+const fs = requrie("fs")
+var result = ""
+
+
+
+async function writeOut(){
+  fs.writeFileSync("./out.json", result)  
+}
+
+async function process(url){
+  
+}
+
+async function handlePage(results){
+  
+}
+
+async function searchApi(){
+  
+}
